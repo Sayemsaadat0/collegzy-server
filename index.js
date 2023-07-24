c15db2a205babd13154821739d5eb3e0f69ee729
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-07-24 14:12:22 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const app = express();
+const port = process.env.port || 5000;
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nuouh7o.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const collegeCollection = client
+      .db("college-server")
+      .collection("colleges");
+    const admissionCollection = client
+      .db("college-server")
+      .collection("admission");
+    app.get("/college", async (req, res) => {
+      const searchTerm = req.query.collegeName;
+
+      const query = searchTerm
+        ? {
+            college_name: { $regex: new RegExp(searchTerm, "i") },
+          }
+        : {};
+      const result = await collegeCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/college/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await collegeCollection.findOne(query);
+      res.send(result);
+    });
+    app.post("/admission", async (req, res) => {
+      const data = req.body;
+      const result = await admissionCollection.insertOne(data);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.log(error));
+
+app.get("/", (req, res) => {
+  res.send("Data base connected SuccessFully");
+});
+app.listen(port, () => {
+  console.log(`Collegy server running on port ${port}`);
+});
